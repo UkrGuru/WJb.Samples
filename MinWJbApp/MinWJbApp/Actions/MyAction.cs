52eb5b74996480eba8d466de9aba21e9aa4eddba
A V <alexander@businessnetsolutions.co.uk>
2025-12-11 18:52:26 +0200

    2025-12

---
+++ MinWJbApp/MinWJbApp/Actions/MyAction.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Nodes;
+using WJb;
+
+namespace Actions;
+
+public class MyAction : IAction
+{
+    private string _name = "World";
+
+    public Task InitAsync(JsonObject more, CancellationToken ct)
+    {
+        _name = more?["name"]?.ToString() ?? _name;
+        return Task.CompletedTask;
+    }
+
+    public Task ExecAsync(CancellationToken ct)
+    {
+        Console.WriteLine($"Hello from MyAction! name={_name}");
+        return Task.CompletedTask;
+    }
+}
---
+++ MinWJbApp/MinWJbApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using Actions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Text.Json.Nodes;
+using WJb;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddTransient<MyAction>();
+
+        services.AddWJb(
+            configureSettings: opts => opts["MaxParallelJobs"] = 2,
+            configureActions: map =>
+            {
+                map["MyAction"] = new ActionItem(
+                    Type: typeof(MyAction).AssemblyQualifiedName!,
+                    More: new JsonObject { ["name"] = "Oleksandr" }
+                );
+            });
+    })
+    .Build();
+
+var jobs = host.Services.GetRequiredService<IJobProcessor>();
+
+// 1) Job that uses ONLY defaults -> prints "Oleksandr"
+var jobDefault = await jobs.CompactAsync("MyAction", new JsonObject()); // or pass null if allowed
+await jobs.EnqueueJobAsync(jobDefault, Priority.High);
+
+// 2) Job that overrides -> prints "Alexander"
+var jobOverride = await jobs.CompactAsync("MyAction", new JsonObject { ["name"] = "Alexander" });
+await jobs.EnqueueJobAsync(jobOverride, Priority.Normal);
+
+await host.RunAsync();
